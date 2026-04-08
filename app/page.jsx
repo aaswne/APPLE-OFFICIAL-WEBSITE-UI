bbe819ffe240463165bf51880516f6c44a0b213f
Aswin Krishna <your_email@example.com>
2026-04-08 10:40:27 +0530

    updated image format

--- app/page.jsx
+++ app/page.jsx
@@ -20,17 +20,17 @@ export default function Home() {
 
       <NavBar show={show} setShow={setShow} />
    
-        <Card description="Meet the latest iPhone lineup." name="iphone" button="Shop Iphone" image="/image/iphone17pros.jpg" baground=" rgb(245, 245, 247)" className="card" />
-        <Card name="MacBook Neo" description="Amazing Mac.Surprising Price." button="Buy" image="/image/macbookneo.jpg" baground=" rgb(245, 245, 247)" className="card" />
-        <Card baground="lightblue" description="Now supercharged by M4" name="ipad Air" button="Buy" image="/image/ipadair.jpg" className="card" />
+        <Card description="Meet the latest iPhone lineup." name="iphone" button="Shop Iphone" image="/image/iphone17pros.webp" baground=" rgb(245, 245, 247)" className="card" />
+        <Card name="MacBook Neo" description="Amazing Mac.Surprising Price." button="Buy" image="/image/macbookneo.webp" baground=" rgb(245, 245, 247)" className="card" />
+        <Card baground="lightblue" description="Now supercharged by M4" name="ipad Air" button="Buy" image="/image/ipadair.webp" className="card" />
 
       <div className="smallCardsRow">
-        <Card image="image/applewatch.jpg" baground=" rgb(245, 245, 247)" button="Buy" description="The ultimate way to Watch your health" name="WATCH SERIES 11" className="smallCard" />
-        <Card image="image/macbookpro.jpg" baground="black" button="Buy" description="Now with M5,M5 Pro and,M5 Max" name="MacBook Pro" color="white" className="smallCard" />
+        <Card image="image/applewatch.webp" baground=" rgb(245, 245, 247)" button="Buy" description="The ultimate way to Watch your health" name="WATCH SERIES 11" className="smallCard" />
+        <Card image="image/macbookpro.webp" baground="black" button="Buy" description="Now with M5,M5 Pro and,M5 Max" name="MacBook Pro" color="white" className="smallCard" />
       </div>
       <div className="smallCardsRow">
-        <Card image="image/applecard.jpg" baground=" rgb(245, 245, 247)" button="Buy" description="Get up to 3% Daily Cash back with every purchase." name="Apple Card" color="black" className="smallCard" />
-        <Card image="image/airpods3pro.jpg" baground=" rgb(245, 245, 247)" button="Buy" description="The world's best in-ear Active Noise Cancellation." name="AirPods 3 Pro" color="black" className="smallCard" />
+        <Card image="image/applecard.webp" baground=" rgb(245, 245, 247)" button="Buy" description="Get up to 3% Daily Cash back with every purchase." name="Apple Card" color="black" className="smallCard" />
+        <Card image="image/airpods3pro.webp" baground=" rgb(245, 245, 247)" button="Buy" description="The world's best in-ear Active Noise Cancellation." name="AirPods 3 Pro" color="black" className="smallCard" />
       </div>
 
       <RawPost url={actions}/>
